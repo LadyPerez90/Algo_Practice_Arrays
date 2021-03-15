18deb707ee44e762d18981e65a38816dbae08861
LadyPerez90 <josefa_groce@yahoo.com>
2021-03-15 00:07:46 -0500

    first commit

---
+++ Groce_Josefa_Algo_Practice_Arrays.js
@@ -0,0 +1,26 @@
+var testArr = [6,3,5,1,2,4]
+
+//Challenge 1
+Num 6, Sum 6
+Num 3, Sum 9
+Num 5, Sum 14
+Num 1, Sum 15
+Num 2, Sum 17
+Num 4, Sum 21
+
+var testArr = [6,3,5,1,2,4]
+let sum=0;
+for(let i=0; i< testArr.length; i++){
+sum += testArr[i];
+console.log("Num", terstArr, "Sum", sum);
+}
+
+
+//Challenge 2
+[0,3,10,3,8,20]
+
+var testArr = [6,3,5,1,2,4]
+for(let i=0; i<testArr.length; i++){
+    testArr[i] = arr[i] * i;
+}
+console.log(testArr);
